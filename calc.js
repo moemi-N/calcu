221358d9c8ef40aebddb838236e16bcf15056efc
moe <pinktulip1027@gmail.com>
2023-10-06 16:45:24 +0900

    javascriptを実行させるうえで何かをまちがっているが分からないので他サイトのものや書き方を試

--- calc.js
+++ calc.js
@@ -1,6 +1,6 @@
 <script>
     function get_calc(btn){
-        if(btn.value =="="){
+        if(btn.value == "=" ){
             document.calc.display.value = eval(document.calc.display.value);
         }else if(btn.value == "C"){
             document.calc.display.value="";
