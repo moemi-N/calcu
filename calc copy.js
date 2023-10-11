4b0a9a2f9654d5956eb8625403923331054ff639
moe <pinktulip1027@gmail.com>
2023-10-11 18:24:01 +0900

    新ブランチ作成

--- calc copy.js
+++ calc copy.js
@@ -1,19 +1,7 @@
 <script>
-    function get_calc(btn){
-        if(btn.value == "=" ){
-            document.calc.display.value = eval(document.calc.display.value);
-        }else if(btn.value == "C"){
-            document.calc.display.value="";
-        }else{
-            if(btn.value == "x"){
-                btn.value ="*";
-            }else if(btn.value =="÷"){
-                btn.value="/";
-            }
-            document.calc.display.value += btn.value;
-            document.calc.multi_btn.value = "×";
-            document.calc.div_btn.value ="÷";
-        }
-        
+    function get_calc(button){
+        let number = button.innerHTML;
+        document.write{"${number}です"}
     }
+
 </script>
